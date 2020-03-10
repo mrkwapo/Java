d1c5ff9b3252727747b168cc16eecfced050314f
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-03-09 20:09:20 -0400

    Create primitiveTypes.java

---
+++ primitiveTypes.java
@@ -0,0 +1,14 @@
+package com.Dracodess;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte age = 35;
+        long viewsCount = 3_123_456_789L;
+        double dPrice = 10.99;
+        float fPrice = 11.99F;
+        char letter = 'A';
+        boolean isEligible = true;
+	System.out.println(age);
+    }
+}
