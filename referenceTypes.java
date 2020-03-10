be1d53338e1259852853445feff0e7c5d7f3dde5
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-03-09 20:16:28 -0400

    Create referenceTypes.java

---
+++ referenceTypes.java
@@ -0,0 +1,12 @@
+package com.Dracodess;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte age = 35; //Primitive types store simple values
+        Date now = new Date(); //Reference types store complex objects
+        System.out.println(now);
+    }
+}
