599aa1ad1797c4f6ef86f4400fdd997e7bd7d331
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-03-09 20:53:11 -0400

    Create strings.java

---
+++ strings.java
@@ -0,0 +1,19 @@
+package com.Dracodess;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String message = "Hello World" + "!!";
+
+        System.out.println(message.startsWith("!!")); //false
+        System.out.println(message.endsWith("!!")); //true
+        System.out.println(message.length()); //13
+        System.out.println(message.indexOf('W')); //6
+        System.out.println(message.replace('!', '*')); //Hello World**  *.replace does not modify the original string
+        System.out.println( message.toLowerCase()); //hello world!!
+
+        String message2 = "   Hello World" + "!!   ";
+        System.out.println(message2.trim()); //Hello World!!
+        System.out.println(message2); //   Hello World!!
+    }
+}
