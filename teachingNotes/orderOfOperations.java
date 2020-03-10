bb3ab87645f556a4c4a38364bb8e67056f4b5169
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-03-10 08:34:40 -0400

    Create orderOfOperations.java

---
+++ teachingNotes/orderOfOperations.java
@@ -0,0 +1,19 @@
+package com.Dracodess;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Order of Operations: parenthesis(), division /, multiplication *, addition +, then subtraction -       
+        int x = 10 + 3 * 2;
+        System.out.println(x); //16
+        
+        int y = (10 + 3) * 2;
+        System.out.println(y); //26
+
+        int z = 10 / 5 * 2;
+        System.out.println(z);//4
+
+        int w = 10 + 5 - 2;
+        System.out.println(w); //13
+    }
+}
