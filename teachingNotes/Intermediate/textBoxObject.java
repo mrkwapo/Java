0f06f0c779a1de8bc2ae6225e79f3a09eeeea975
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-03-15 21:21:03 -0400

    Create textBoxObject.java

---
+++ teachingNotes/Intermediate/textBoxObject.java
@@ -0,0 +1,14 @@
+package com.Dracodess;
+public class Main {
+
+    public static void main(String[] args) {
+
+        TextBox textBox1 = new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text.toUpperCase());
+
+        TextBox textBox2 = new TextBox();
+        textBox2.setText("Box 2");
+        System.out.println(textBox2.text);
+    }
+}
