2a6879d76daf82a4982e584eb159319cb20267cc
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-03-15 21:08:08 -0400

    created 3 members

---
+++ teachingNotes/Intermediate/textBoxClass.java
@@ -0,0 +1,14 @@
+package com.Dracodess;
+
+public class TextBox {
+    public String text; //Field
+
+    //Methods
+    public void setText(String text){
+        this.text = text;
+    }
+    public void clear() {
+        text = "";
+    }
+
+}
