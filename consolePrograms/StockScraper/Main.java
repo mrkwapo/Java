2c26ec64aff1f581573e837e9e31865ceb9526b4
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-03-20 09:55:21 -0400

    Update Main.java

--- consolePrograms/StockScraper/Main.java
+++ consolePrograms/StockScraper/Main.java
@@ -8,11 +8,10 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:/Users/Kwapalapagus/Desktop/Chrome/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        var yahoo = new yahooWatchlist();
-        yahoo.getWatchlist(driver);
-
-        var data = new Data();
-        data.ParseData(driver);
+        yahooWatchlist yahoo = new yahooWatchlist();
+        yahoo.login(driver);
+        Data data = new Data();
+        data.displayStockData(driver);
         driver.close();
     }
 }
