6437987190ded5518df1d2dbeeeb33a1f6b779e8
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-03-22 15:16:30 -0400

    Update Main.java

--- consolePrograms/TSPTracker/Main.java
+++ consolePrograms/TSPTracker/Main.java
@@ -1,12 +1,8 @@
 package com.Dracodess;
-
-import org.openqa.selenium.NoSuchSessionException;
-
 public class Main {
-
-    public static void main(String[] args) throws NoSuchSessionException {
-        var sharePrices = new SharePrices();
-        sharePrices.getCurrentPricesOfToday();
-        sharePrices.getPast25daysOfPrices();
+    public static void main(String[] args) {
+        var sharePrices = new ShareData();
+        sharePrices.displayCurrentPricesOfToday();
+        sharePrices.displayPast25daysOfPrices();
     }
 }
