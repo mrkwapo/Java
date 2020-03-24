098deef810f4de5a3fdc2bbab218a23563a675f3
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-03-24 08:24:10 -0400

    Update Main.java

--- consolePrograms/TSPTracker/Main.java
+++ consolePrograms/TSPTracker/Main.java
@@ -2,7 +2,7 @@ package com.Dracodess;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","C:/Users/Kwapalapagus/Desktop/Chrome/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:/YourPathTo/chromedriver.exe");
         var sharePrices = new ShareData();
         sharePrices.displayCurrentPricesOfToday();
         sharePrices.displayPast25daysOfPrices();
