36d6af9473f816592c68dfa9727e418c4e38f94d
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-03-26 08:52:23 -0400

    more succinct
    Used encapsulation and abstraction to make code more succinct.

--- consolePrograms/TSPTracker/Main.java
+++ consolePrograms/TSPTracker/Main.java
@@ -4,8 +4,6 @@ import java.text.ParseException;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-        System.setProperty("webdriver.chrome.driver","C:/Users/Kwapalapagus/Desktop/Chrome/chromedriver.exe");
-        var shareData = new ShareData();
-        ShareReport.displaySharePricesByDateRange(shareData);
+        ShareReport.displaySharePricesByDateRange();
     }
 }
