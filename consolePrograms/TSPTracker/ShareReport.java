b9dc5f3f380784cf5196c3be2a4c46c915ae0266
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-03-27 23:39:22 -0400

    Update ShareReport.java

--- consolePrograms/TSPTracker/ShareReport.java
+++ consolePrograms/TSPTracker/ShareReport.java
@@ -1,17 +1,15 @@
 package com.Dracodess;
 
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ShareReport {
 
     //This method displays share data of a specified date or date range
     public static void displaySharePricesByDateRange() throws ParseException {
-        ShareData shareData = new ShareData();
-        shareData.navigateToPage();
-        shareData.inputDateRange();
-        var prices = shareData.getShareData();
+        Scraper scraper = new Scraper();
+        scraper.navigateToPage();
+        scraper.inputDateRange();
+        var prices = scraper.getShareData();
 
         System.out.println();
         System.out.println("______________________");
