b3174b7c06011d1155fe03c1c8b14993b681e673
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-03-28 01:33:51 -0400

    Update Scraper.java

--- consolePrograms/TSPTracker/Scraper.java
+++ consolePrograms/TSPTracker/Scraper.java
@@ -11,33 +11,24 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.text.ParseException;
 import java.util.List;
-import java.util.Scanner;
 
 /* The concern of this class is to handle scraping data */
 public class Scraper {
     //Setting up and instantiating Chrome WebDriver to run in headless mode
-    public String PATH = System.setProperty("webdriver.chrome.driver","C:/Users/YourPathHere/chromedriver.exe");
+    private String PATH = System.setProperty("webdriver.chrome.driver","C:/Users/YourPathHere/chromedriver.exe");
     private ChromeOptions options = new ChromeOptions().addArguments("--headless");
     private WebDriver driver = new ChromeDriver(options);
-
-    //Using scanner to read user input
-    private Scanner scanner = new Scanner(System.in);
 
     //Using Selenium's Wait Class to handle timing
     private WebDriverWait wait = new WebDriverWait(driver, 20000);
 
-    public void navigateToPage(){
+    private void navigateToPage(){
         //Using Web Driver to Navigate to the TSP Page
         driver.get("https://www.tsp.gov/InvestmentFunds/FundPerformance/index.html");
     }
-    public List<WebElement> getShareData(){
-        //Selecting Table Data
-        List<WebElement> data = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("tspStandard"))).findElements(By.tagName("td"));
-        return data;
-    }
 
     //This method handles, entering the date range and clicking the button to retrieve data of the selected date range
-    public void inputDateRange() throws ParseException {
+    private void inputDateRange() throws ParseException {
         var startDateInputBox =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("startdate")));
         var endDateInputBox =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("enddate")));
 
@@ -46,5 +37,15 @@ public class Scraper {
 
         var retrieveSharesBtn = driver.findElement(By.id("retrieve"));
         retrieveSharesBtn.sendKeys(Keys.RETURN);
+    }
+
+    //Getter
+    public List<WebElement> getShareData() throws ParseException {
+        navigateToPage();
+        inputDateRange();
+
+        //Selecting Table Data
+        List<WebElement> data = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("tspStandard"))).findElements(By.tagName("td"));
+        return data;
     }
 }
