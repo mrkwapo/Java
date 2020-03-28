fc31ef9c60d132ee0d253295c49071f3afa5cef2
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-03-28 00:33:03 -0400

    Update and rename ShareReport.java to Report.java

--- consolePrograms/TSPTracker/Report.java
+++ consolePrograms/TSPTracker/Report.java
@@ -2,11 +2,16 @@ package com.Dracodess;
 
 import java.text.ParseException;
 
-public class ShareReport {
+public class Report {
+
+   private static Scraper scraper = new Scraper();
+
+    public Report(Scraper scraper) {
+        this.scraper = scraper;
+    }
 
     //This method displays share data of a specified date or date range
-    public static void displaySharePricesByDateRange() throws ParseException {
-        Scraper scraper = new Scraper();
+    public static void getSharePricesByDateRange() throws ParseException {
         scraper.navigateToPage();
         scraper.inputDateRange();
         var prices = scraper.getShareData();
@@ -15,6 +20,7 @@ public class ShareReport {
         System.out.println("______________________");
         System.out.println("SHARE PRICE REPORT:");
         System.out.println("______________________");
+
         for (var item : prices)
             System.out.println(item.getText());
     }
