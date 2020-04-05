d49c777392311c1cd36f824808c1061537fe80f3
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-04-04 22:36:25 -0400

    Create UIControl.java

---
+++ teachingNotes/Advanced/AbstractClassesAndMethods/UIControl.java
@@ -0,0 +1,19 @@
+package com.Dracodess;
+
+# public abstract class UIControl {
+    protected boolean isEnabled = true;
+
+# public abstract void render();
+
+    public void enable(){
+        isEnabled = true;
+    }
+
+    public void disable(){
+        isEnabled = false;
+    }
+
+    public boolean isEnabled(){
+        return isEnabled;
+    }
+}
