4b8a9dd19c0cba85e21ad81aeb2ce284cfd9ad38
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-04-04 22:25:05 -0400

    Create CheckBox.java

---
+++ teachingNotes/Advanced/Polymorphism/CheckBox.java
@@ -0,0 +1,8 @@
+package com.Dracodess;
+
+public class CheckBox extends UIControl {
+    @Override
+    public void render() {
+        System.out.println("Render CheckBox"); //ENcapsulating a render method for each class
+    }
+}
