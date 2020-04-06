c19d47b7657df8f4b1e69fcecadd6591b9df117f
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-04-06 14:31:04 -0400

    Create Video.java

---
+++ teachingNotes/Intermediate/Decoupling/mytube/Video.java
@@ -0,0 +1,24 @@
+package com.Dracodess;
+
+public class Video {
+    public Object getUser() {
+        Object user = null;
+        return user;
+    }
+
+    public String getTitle() {
+        return null;
+    }
+
+    public void setFileName(String s) {
+
+    }
+
+    public void setTitle(String s) {
+
+    }
+
+    public void setUser(User user) {
+    }
+}
+
