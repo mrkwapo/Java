a127cfa912b74c151ed73880a28c58429061fb46
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-04-05 22:03:03 -0400

    Create TaxReport.java

---
+++ teachingNotes/Intermediate/Interfaces/Dependency Injection/Constructor Injection/TaxReport.java
@@ -0,0 +1,15 @@
+package com.Dracodess;
+
+public class TaxReport {
+    private TaxCalculator calculator;
+
+    //Constructor Injection
+    public TaxReport(TaxCalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public void show() {
+        var tax = calculator.calculateTax();
+        System.out.println(tax);
+    }
+}
