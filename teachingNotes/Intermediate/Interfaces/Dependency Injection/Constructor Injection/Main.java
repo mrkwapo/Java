1d3c70584248932c72862c657f9529607fd52b08
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-04-05 22:02:25 -0400

    Create Main.java

---
+++ teachingNotes/Intermediate/Interfaces/Dependency Injection/Constructor Injection/Main.java
@@ -0,0 +1,11 @@
+package com.Dracodess;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var calculator = new TaxCalculator2018(100_000);
+        var report = new TaxReport(calculator);
+    }
+}
+
+
