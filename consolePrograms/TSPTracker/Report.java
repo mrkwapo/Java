1e8484fe42300f419235bfb1c503b17551353287
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-04-09 02:12:18 -0400

    Update Report.java

--- consolePrograms/TSPTracker/Report.java
+++ consolePrograms/TSPTracker/Report.java
@@ -1,18 +1,11 @@
 package com.Dracodess;
 
-import java.text.ParseException;
-
 //The concern of this class is to handle displaying a report of retrieved shares
 public class Report {
-
-   private static Scraper scraper = new Scraper();
-
-    public Report(Scraper scraper) {
-        this.scraper = scraper;
-    }
+    private static Scraper scraper = new ScraperSpring2020();
 
     //This method displays share data of a specified date or date range
-    public static void getSharePricesByDateRange() throws ParseException {
+    public static void getSharePricesByDateRange() {
         var prices = scraper.getShareData();
         System.out.println();
         System.out.println("______________________");
