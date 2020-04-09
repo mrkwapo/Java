b1d7d29b8d8dfbac9d28f08183e4d5ae8ccf6ac0
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-04-09 02:17:57 -0400

    Update Main.java

--- consolePrograms/StockScraper/Main.java
+++ consolePrograms/StockScraper/Main.java
@@ -1,17 +1,8 @@
 package com.Dracodess;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver","C:/Users/YourPathTo/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        yahooWatchlist yahoo = new yahooWatchlist();
-        yahoo.login(driver);
-        Data data = new Data();
-        data.displayStockData(driver);
-        driver.close();
+    public static void main(String[] args) {
+        Report.displayStockData();
     }
 }
